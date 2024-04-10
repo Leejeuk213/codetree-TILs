2f510319cd36534bc46b719b1be455ba5afbd48a
Leejeuk213 <73926434+Leejeuk213@users.noreply.github.com>
2024-04-11 02:46:31 +0900

    Title: 왕실의 기사 대결, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240411/왕실의 기사 대결/royal-knight-duel.cpp
+++ 240411/왕실의 기사 대결/royal-knight-duel.cpp
@@ -1,193 +1,113 @@
-#include<iostream>
-#include<map>
-#include<queue>
-#include<vector>
+#include <iostream>
+#include <queue>
+
 using namespace std;
 
-int board[41][41] ;
-int l,n,q;
+#define MAX_N 31
+#define MAX_L 41
 
-int sum_ok = 1;
+int l, n, q;
+int info[MAX_L][MAX_L];
+int bef_k[MAX_N];
+int r[MAX_N], c[MAX_N], h[MAX_N], w[MAX_N], k[MAX_N];
+int nr[MAX_N], nc[MAX_N];
+int dmg[MAX_N];
+bool is_moved[MAX_N];
 
-map <int , pair<int,int>> knight;
-map <int , pair<int,int>> knight_land;
-map <int , int> knight_state;
-map <int , int> knight_hp;
-map <int , int> knight_score;
-map <int , int> knight_pushed;
+int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, 1, 0, -1};
 
-int dx[4]={-1,0,1,0};
-int dy[4]={0,1,0,-1};
-
-void to_sum(int i){
-
-    int f_x = knight[i].first;
-    int f_y = knight[i].second;
-    int r_x = knight[i].first + knight_land[i].first - 1;
-    int r_y = knight[i].second + knight_land[i].second - 1;
-    int sum = 0;
-    for(int j=f_x; j<=r_x;j++){
-        for(int k=f_y;k<=r_y;k++){
-            if(board[j][k] == 1) sum++;
-        }
+// 움직임을 시도해봅니다.
+bool TryMovement(int idx, int dir) {
+    // 초기화 작업입니다.
+    for(int i = 1; i <= n; i++) {
+        dmg[i] = 0;
+        is_moved[i] = false;
+        nr[i] = r[i];
+        nc[i] = c[i];
     }
-    knight_score[i] += sum;
-    knight_hp[i] -= sum;
-    if(knight_hp[i] <=0) knight_state[i] = 1;
-    return ;
-}
-vector<int> is_knight(int x1, int y1, int x2, int y2, int cur){
 
-    vector<int> qe;
-    for(int i=1; i<=n;i++){
-        //if(i == cur) continue;
-        if(knight_state[i] == 1) continue;
-        int f_x = knight[i].first;
-        int f_y = knight[i].second;
-        int r_x = knight[i].first + knight_land[i].first - 1;
-        int r_y = knight[i].second + knight_land[i].second - 1;
-        int ok = 0;
-        for(int j=x1; j<=x2;j++){
-            for(int k=y1;k<=y2;k++){
-                if( (j >= f_x &&  j<= r_x) && (k >= f_y && k <= r_y) ) {
-                    ok = 1;
-                    break;
-                }
-            }
-            if(ok){
-                qe.push_back(i);
-                break;
+    queue<int> q;
+
+    q.push(idx);
+    is_moved[idx] = true;
+
+    while(!q.empty()) {
+        int x = q.front(); q.pop();
+
+        nr[x] += dx[dir];
+        nc[x] += dy[dir];
+
+        // 경계를 벗어나는지 체크합니다.
+        if(nr[x] < 1 || nc[x] < 1 || nr[x] + h[x] - 1 > l || nc[x] + w[x] - 1 > l)
+            return false;
+
+        // 대상 조각이 다른 조각이나 장애물과 충돌하는지 검사합니다.
+        for(int i = nr[x]; i <= nr[x] + h[x] - 1; i++) {
+            for(int j = nc[x]; j <= nc[x] + w[x] - 1; j++) {
+                if(info[i][j] == 1) 
+                    dmg[x]++;
+                if(info[i][j] == 2)
+                    return false;
             }
         }
-    }
-    return qe;
-}
 
-int is_wall(int x1, int y1, int x2, int y2){
+        // 다른 조각과 충돌하는 경우, 해당 조각도 같이 이동합니다.
+        for(int i = 1; i <= n; i++) {
+            if(is_moved[i] || k[i] <= 0) 
+                continue;
+            if(r[i] > nr[x] + h[x] - 1 || nr[x] > r[i] + h[i] - 1) 
+                continue;
+            if(c[i] > nc[x] + w[x] - 1 || nc[x] > c[i] + w[i] - 1) 
+                continue;
 
-    for(int i=x1;i<=x2;i++){
-        for(int j=y1;j<=y2;j++){
-            if(board[i][j] == 2) return 1;
+            is_moved[i] = true;
+            q.push(i);
         }
     }
-    return 0;
+
+    dmg[idx] = 0;
+    return true;
 }
 
-void knight_mv(int target_knight, int v){
+// 특정 조각을 지정된 방향으로 이동시키는 함수입니다.
+void MovePiece(int idx, int dir) {
+    if(k[idx] <= 0) 
+        return;
 
-    if(knight_state[target_knight] == 1) return ;
-
-    int f_x = knight[target_knight].first;
-    int f_y = knight[target_knight].second;
-    int r_x = knight[target_knight].first + knight_land[target_knight].first  - 1;
-    int r_y = knight[target_knight].second + knight_land[target_knight].second - 1; 
-
-    int mv_f_x = f_x + dx[v];
-    int mv_f_y = f_y + dy[v];
-    int mv_r_x = r_x + dx[v];
-    int mv_r_y = r_y + dy[v];
-
-    // 이동하는게 벽을 넘는지
-    if(mv_f_x > l || mv_f_x <=0 || mv_f_y > l || mv_f_y <=0 || mv_r_x > l || mv_r_x <=0 || mv_r_y > l || mv_r_y <=0){
-        sum_ok = 0;
-        return ;
-    }
-    if(is_wall(mv_f_x,mv_f_y,mv_r_x,mv_r_y)){
-        sum_ok = 0;
-        return ;
-    }
-
-    // 이동하는데 상대 기사가 있는지
-    vector<int> qe = is_knight(mv_f_x,mv_f_y,mv_r_x,mv_r_y,target_knight);
-    if(!qe.empty()){
-        for(int i = 0; i<qe.size();i++){
-            //cout << qe[i] <<' ';
-            knight_pushed[qe[i]] = 1;
-            knight_mv(qe[i],v);
+    // 이동이 가능한 경우, 실제 위치와 체력을 업데이트합니다.
+    if(TryMovement(idx, dir)) {
+        for(int i = 1; i <= n; i++) {
+            r[i] = nr[i];
+            c[i] = nc[i];
+            k[i] -= dmg[i];
         }
-        //cout<<'\n';
     }
-    
-    // 없다면 해당 기사 움직임 적용시키고 리턴
-    else{
-        knight[target_knight].first = mv_f_x;
-        knight[target_knight].second = mv_f_y;
-        return;
-    }
-    if(sum_ok){
-        knight[target_knight].first = mv_f_x;
-        knight[target_knight].second = mv_f_y;
-        return;
-    }
-    return;
 }
 
-int main(){
-
+int main() {
+    // 입력값을 받습니다.
     cin >> l >> n >> q;
+    for(int i = 1; i <= l; i++)
+        for(int j = 1; j <= l; j++)
+            cin >> info[i][j];
+    for(int i = 1; i <= n; i++) {
+        cin >> r[i] >> c[i] >> h[i] >> w[i] >> k[i];
+        bef_k[i] = k[i];
+    }
+    for(int i = 1; i <= q; i++) {
+        int idx, dir;
+        cin >> idx >> dir;
+        MovePiece(idx, dir);
+    }
 
-    for(int i= 1; i<=l;i++){
-
-        for(int j = 1; j<=l;j++){
-            cin >> board[i][j];
+    // 결과를 계산하고 출력합니다.
+    long long ans = 0;
+    for(int i = 1; i <= n; i++) {
+        if(k[i] > 0) {
+            ans += bef_k[i] - k[i];
         }
     }
 
-
-    for(int i= 1 ; i<= n; i++){
-        int r , c , h ,w ,k;
-        cin >> r >> c >> h >> w >>k;
-        knight[i].first = r; 
-        knight[i].second = c; 
-
-        knight_land[i].first = h;
-        knight_land[i].second = w;
-
-        knight_hp[i] = k;
-
-        knight_score[i] = 0;
-        knight_state[i] = 0;
-        knight_pushed[i] = 0;
-    }
-
-    for(int i= 0; i < q; i++){
-        sum_ok = 1;
-        int target_knight, v ;
-        cin >> target_knight >> v;
-        knight_mv(target_knight,v);
-
-        if(sum_ok){
-            for(int j=1;j<=n;j++){
-                if(knight_pushed[j]==1){
-                    knight_pushed[j] = 0;
-                    to_sum(j);
-                }
-            }
-        }
-        else{
-            for(int j=1;j<=n;j++){
-                if(knight_pushed[j]==1){
-                    knight_pushed[j] = 0;
-                }
-            }
-        }
-
-        // cout << '\n';
-        // for(int j=1;j<=n;j++){
-        //     cout << knight[j].first <<' ' << knight[j].second << '\n'; 
-        //     cout << knight_hp[j] << ' ' << knight_score[j] << '\n';
-        // }
-        // cout<< '\n';
-    }
-
-    int cnt = 0;
-    for(int i = 1; i <= n; i++){
-
-        if(knight_state[i] != 0) continue;
-        //cout << knight_score[i] <<'\n';
-        cnt += knight_score[i];
-    }
-
-    cout << cnt;
+    cout << ans;
     return 0;
 }
